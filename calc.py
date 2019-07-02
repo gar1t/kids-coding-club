003861f2bece6b364c08e6093720ed0fcfa4124f
Garrett Smith <g@rre.tt>
2019-07-02 12:16:53 -0500

    Update with remaining ops

--- calc.py
+++ calc.py
@@ -2,18 +2,47 @@ import sys
 
 def main():
     while True:
-        calculate()
+        try:
+            calculate()
+        except StopIteration:
+            break
 
 def calculate():
     operation = read_operation()
+    if operation == 'q':
+        raise StopIteration()
+    dispatch_operation(operation)
+
+def dispatch_operation(op):
+    if op == 'a':
+        add()
+    elif op == 's':
+        subtract()
+    elif op == 'm':
+        multiply()
+    elif op == 'd':
+        divide()
+    else:
+        assert False, op
+
+def add():
     x = read_var('x')
     y = read_var('y')
     answer = x + y
     write_answer(answer, x, y)
 
+def subtract():
+    pass
+
+def multiply():
+    pass
+
+def divide():
+    pass
+
 def read_operation():
     write_operation_prompt()
-    return read_input()
+    return input_to_operation(read_input())
 
 def write_operation_prompt():
     sys.stdout.write('What would you like to do?\n')
@@ -24,6 +53,13 @@ def write_operation_prompt():
     sys.stdout.write(' [q]uit\n')
     sys.stdout.write('> ')
     sys.stdout.flush()
+
+def input_to_operation(s):
+    op = s.strip()
+    valid = ('a', 's', 'm', 'd', 'q')
+    if op not in valid:
+        raise ValueError('invalid operation - must be in {}'.format(valid))
+    return op
 
 def read_var(var_name):
     write_var_prompt(var_name)
@@ -45,7 +81,7 @@ def write_var_prompt(var_name):
     sys.stdout.flush()
 
 def write_answer(answer, x, y):
-    sys.stdout.write('{} + {} = {}\n\n'.format(x, y, answer))
+    sys.stdout.write('\n----> {} + {} = {}\n\n'.format(x, y, answer))
 
 if __name__ == '__main__':
     main()
